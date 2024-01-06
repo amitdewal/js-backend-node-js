c0854298d5cc01c94dc399e99b1221dcfe970cac
Amit Dewal <daredewal007@gmail.com>
2024-01-06 15:07:22 +0530

    util code Api response Error response

--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@ dotenv.config({
 // second approach to connect DB
 
 connectDB()
+.then(() =>{
+    app.listen(process.env.PORT || 8000,() =>{
+        console.log(`Server is running at PORT : ${process.env.PORT}`);
+    })
+})
+.catch((err) =>{
+    console.log("MONGO db connection failed !!!", err)
+})
 
 
 
